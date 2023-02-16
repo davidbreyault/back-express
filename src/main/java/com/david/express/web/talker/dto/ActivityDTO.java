573b4c733943fae9aad01aef28c33fb1e2985fb5
David Breyault <davidbreyault@gmail.com>
2023-02-16 10:37:48 +0100

    [DEV11] Point d'API GET pour les utilisateurs les plus actifs

---
+++ src/main/java/com/david/express/web/talker/dto/ActivityDTO.java
@@ -0,0 +1,12 @@
+package com.david.express.web.talker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ActivityDTO {
+
+    private int notes;
+    private int comments;
+}
